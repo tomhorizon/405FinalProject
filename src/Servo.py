c6c086a0a4927674c5acab634c1b1f2c4893dded
jwfraserpoly <jwfraser@calpoly.edu>
2023-03-09 13:35:33 -0800

    class

--- src/Servo.py
+++ src/Servo.py
@@ -1,23 +1,23 @@
 import pyb
 import utime
 
-
-servo_signal = pyb.Timer(5, freq=250)
-servo_pin = pyb.Pin(pyb.Pin.board.PA3, pyb.Pin.OUT_PP)
-signal_ch = servo_signal.channel(4, pyb.Timer.PWM, pin=servo_pin, pulse_width=8000)
+class Servo
     
     
-    
-def fire():
-    print('8000')
-    signal_ch.pulse_width(8000)
-    pyb.delay(350)
-    print('16000')
-    signal_ch.pulse_width(19000)
-    pyb.delay(350)
-    
+    def __init__(self, pin1, tim_num, chan_num)
+    self.servo_signal = pyb.Timer(tim_num, freq=250)
+    self.servo_pin = pyb.Pin(pin1, pyb.Pin.OUT_PP)
+    self.signal_ch = self.servo_signal.channel(chan_num, pyb.Timer.PWM, pin=servo_pin, pulse_width=8000)
+        
+        
+        
+    def fire(self):
+        self.signal_ch.pulse_width(8000)
+        pyb.delay(350)
+        self.signal_ch.pulse_width(19000)
+        pyb.delay(350)
+        
 if __name__ == '__main__':
-    while True:
-        fire()
+    pass
         
 
